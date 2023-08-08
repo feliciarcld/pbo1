f00ad31228aa658cb3649f90e0256ff458b1a89f
ulfijulianti <ulfijulianti07@gmail.com>
2023-08-08 08:07:32 +0700

    felicia.pbo1

---
+++ class/class_perhitungan.php
@@ -0,0 +1,40 @@
+<?php
+
+//Membuat class
+class perhitungan{
+
+    //property
+    var $sisi, $sisi1, $sisi2, $sisi3;
+    var $alas, $tinggi;
+    var $panjang, $lebar;
+    var $hasil;
+
+    // Method merupakan suatu operasi berupa fungsi-fungsi yang dapat dikerjakan
+    // oleh suatu object. Method didefinisikan pada class akan tetapi dipanggil melalui object
+    //method
+    function LuasPersegi($sisi){
+        $hasil=$sisi*$sisi;
+        return "Hasil Luas Persegi : ".$hasil."<br>";
+    }
+
+    function LuasPersegiPanjang($panjang, $lebar) {
+        $hasil = $panjang * $lebar;
+        return "Hasil Luas Persegi Panjang : " . $hasil . "<br>";
+    }
+
+    function LuasSegitiga($alas, $tinggi){
+        $hasil=$alas*$tinggi/2;
+        return "Hasil Luas Segitiga : ".$hasil."<br>";
+    }
+}
+
+?>
+
+
+
+
+
+
+
+}
+?>
---
+++ view/view_segitiga.php
@@ -0,0 +1,9 @@
+<?php
+
+require('../class/class_perhitungan.php');
+
+$segitiga = new perhitungan();
+
+echo $segitiga->LuasSegitiga(6, 20);
+
+?>
